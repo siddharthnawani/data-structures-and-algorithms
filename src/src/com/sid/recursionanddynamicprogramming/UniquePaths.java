6222e932416efa0f1a3725f4c8cc4bc3ae0cbed3
siddharthnawani <siddharth.nawani@gmail.com>
2020-08-07 02:35:12 +0530

    Added recursion, tabulation and memoization for UniquePaths

--- src/src/com/sid/recursionanddynamicprogramming/UniquePaths.java
+++ src/src/com/sid/recursionanddynamicprogramming/UniquePaths.java
@@ -1,35 +1,83 @@
 package src.com.sid.recursionanddynamicprogramming;
 
-// 62. Unique Paths
+/**
+ *
+ *62. Unique Paths
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ *
+ * How many possible unique paths are there?
+ *
+ * Example 1:
+ *
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ *
+ * Example 2:
+ *
+ * Input: m = 7, n = 3
+ * Output: 28
+ *
+ * **/
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UniquePaths {
 
-    public static int uniquePaths(int m, int n) {
-        Map<String,Integer> hm=new HashMap<>();
-        return getWays(n,m,0,0,hm);
+    int getWaysByTabulation(int si, int sj, int ei, int ej) {
+        int[][] dp = new int[ei][ej];
+
+        for (int i = ei - 1; i >= 0; i--) {
+            for (int j = ej - 1; j >= 0; j--) {
+
+                if (i == ei - 1 || j == ej - 1)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
     }
 
-    static int getWays(int r, int c, int i, int j, Map<String,Integer> hm){
+    int getWaysByRecursion(int si, int sj, int ei, int ej) {
+        if (si == ei && sj == ej)
+            return 1;
+        if (si > ei || sj > ej) return 0;
+        int op1 = getWaysByRecursion(si + 1, sj, ei, ej);
+        int op2 = getWaysByRecursion(si, sj + 1, ei, ej);
+        return op1 + op2;
+    }
 
-        if(i >= r || i<0 || j >=c || j<0)
-            return 0;
-        String key=i+"#"+j;
-        if(hm.containsKey(key))
+    int getWaysByMemoization(int si, int sj, int ei, int ej, Map<String, Integer> hm) {
+        if (si == ei && sj == ej) return 1;
+        if (si > ei || sj > ej) return 0;
+
+        String key = si + "#" + sj;
+        if (hm.containsKey(key))
             return hm.get(key);
 
-        if(i==r-1 && j==c-1)
-            return 1;
+        int op1 = getWaysByMemoization(si + 1, sj, ei, ej, hm);
+        int op2 = getWaysByMemoization(si, sj + 1, ei, ej, hm);
+        hm.put(key, op1 + op2);
 
-        int res= getWays(r,c,i+1,j,hm) + getWays(r,c,i,j+1,hm);
-        hm.put(key,res);
-        return res;
+        return op1 + op2;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(uniquePaths(3,3));
+        int m = 3, n = 2;
+        System.out.println(new UniquePaths().getWaysByRecursion(0, 0, m - 1, n - 1));
+        System.out.println(new UniquePaths().getWaysByMemoization(0, 0, m - 1, n - 1, new HashMap<>()));
+        System.out.println(new UniquePaths().getWaysByTabulation(0, 0, m, n));
     }
+
+
 }
