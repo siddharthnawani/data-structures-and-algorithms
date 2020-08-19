6a884920b1395e4843c75dc0924ab9890c9ac657
siddharthnawani <siddharth.nawani@gmail.com>
2020-08-19 23:07:28 +0530

    Added Surrounded Regions

---
+++ src/src/com/sid/graphs/SurroundedRegions.java
@@ -0,0 +1,78 @@
+package src.com.sid.graphs;
+
+/**
+ * 130. Surrounded Regions
+ * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+ * <p>
+ * A region is captured by flipping all 'O's into 'X's in that surrounded region.
+ * <p>
+ * Example:
+ * <p>
+ * X X X X
+ * X O O X
+ * X X O X
+ * X O X X
+ * After running your function, the board should be:
+ * <p>
+ * X X X X
+ * X X X X
+ * X X X X
+ * X O X X
+ * Explanation:
+ * <p>
+ * Surrounded regions shouldn’t be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+ **/
+public class SurroundedRegions {
+
+    public void solve(char[][] board) {
+        //Simple corner case but gives us improvement of 2 ms
+        if (board.length < 2) return;
+        //The plan is to go to the outer boundary and do dfs if we find O and convert them into # and then the remaining O are simply X
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                //only travserse the boundary
+                if ((i == 0 || i == board.length - 1 || j == 0 || j == board[0].length - 1) && board[i][j] == 'O') {                      //convert them to something else so that they can be marked
+                    dfs(board, i, j);
+                }
+            }
+        }
+
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '#') board[i][j] = 'O';
+                else if (board[i][j] == 'O') board[i][j] = 'X';
+
+            }
+        }
+
+
+    }
+
+    void dfs(char[][] board, int r, int c) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != 'O')
+            return;
+        board[r][c] = '#';
+        dfs(board, r + 1, c);
+        dfs(board, r - 1, c);
+        dfs(board, r, c + 1);
+        dfs(board, r, c - 1);
+
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'}
+        };
+        new SurroundedRegions().solve(board);
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++)
+                System.out.print(board[i][j]);
+            System.out.println();
+        }
+    }
+}
