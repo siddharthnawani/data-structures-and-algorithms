d4afb7dbfed95a663ec32dd255d69a1482afd2a5
siddharthnawani <siddharth.nawani@gmail.com>
2020-08-30 15:21:28 +0530

    Added Print Increasing using recursion

---
+++ src/src/com/sid/recursionanddynamicprogramming/recursion/PrintIncreasing.java
@@ -0,0 +1,30 @@
+package src.com.sid.recursionanddynamicprogramming.recursion;
+
+/***
+ *
+ * https://www.youtube.com/watch?v=Y5IobAWmfjQ&list=PL-Jc9J83PIiFxaBahjslhBD1LiJAV7nKs&index=2
+ *
+ * Print Increasing numbers using recursion
+ *
+ * **/
+public class PrintIncreasing {
+    private void printIncresing(int n){
+        /**
+         * Code which is written above always gets executed when recursion is going up to the stack ie
+         * when recursive calls are being submitted
+         * **/
+        if(n==0) return;
+        printIncresing(n-1);
+        /***
+         * Code written after the recursive call gets executed when the recursion is coming down from
+         * thw stack.
+         * **/
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        new PrintIncreasing().printIncresing(n);
+    }
+
+}
