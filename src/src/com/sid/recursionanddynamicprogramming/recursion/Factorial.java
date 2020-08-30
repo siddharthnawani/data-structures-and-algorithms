7906724bbe08fe1b8e4184ab9e8f05f15eeb2ed1
siddharthnawani <siddharth.nawani@gmail.com>
2020-08-30 16:04:08 +0530

    Added Factorial

---
+++ src/src/com/sid/recursionanddynamicprogramming/recursion/Factorial.java
@@ -0,0 +1,19 @@
+package src.com.sid.recursionanddynamicprogramming.recursion;
+
+/**
+ * https://www.pepcoding.com/resources/online-java-foundation/introduction-to-recursion/factorial-official/ojquestion
+ * Question
+ * 1. You are given a number n.
+ * 2. You are required to calculate the factorial of the number. Don't change the signature of factorial function.
+ ***/
+public class Factorial {
+    public static void main(String[] args) throws Exception {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) return n;
+        return n * factorial(n - 1);
+    }
+}
