7579c169448b780485e9b3f64d418db3eaf6069f
siddharthnawani <siddharth.nawani@gmail.com>
2020-08-30 15:23:21 +0530

    Added Print Decreasing

---
+++ src/src/com/sid/recursionanddynamicprogramming/recursion/PrintDecreasing.java
@@ -0,0 +1,14 @@
+package src.com.sid.recursionanddynamicprogramming.recursion;
+
+public class PrintDecreasing {
+    private void printDecreasing(int n){
+        if(n==0) return;
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        new PrintDecreasing().printDecreasing(n);
+    }
+}
