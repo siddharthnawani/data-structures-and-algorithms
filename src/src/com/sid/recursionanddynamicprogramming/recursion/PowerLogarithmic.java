1020d5feb26e4d8244d233eef70d89fe9100e07a
siddharthnawani <siddharth.nawani@gmail.com>
2020-08-30 16:35:30 +0530

    Added Power Logarithmic

---
+++ src/src/com/sid/recursionanddynamicprogramming/recursion/PowerLogarithmic.java
@@ -0,0 +1,26 @@
+package src.com.sid.recursionanddynamicprogramming.recursion;
+
+/***
+ *https://www.pepcoding.com/resources/online-java-foundation/introduction-to-recursion/power-logarithmic-official/ojquestion
+ *
+ * Question
+ * 1. You are given a number x.
+ * 2. You are given another number n.
+ * 3. You are required to calculate x raised to the power n. Don't change the signature of power function.
+ *
+ * ***/
+public class PowerLogarithmic {
+    public static void main(String[] args) throws Exception {
+        int x = 2, n = 5;
+        System.out.println(power(x, n));
+    }
+
+    public static int power(int x, int n) {
+        if (n == 0) return 1;
+        int xnb2 = power(x, n / 2);
+        int xn = xnb2 * xnb2;
+        if (n % 2 == 1)
+            xn *= x;
+        return xn;
+    }
+}
