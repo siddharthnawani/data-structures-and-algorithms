fc50787234638346e947086d804d501b22bf5442
siddharthnawani <siddharth.nawani@gmail.com>
2020-09-02 23:05:10 +0530

    Added Get Subsequence

---
+++ src/src/com/sid/recursionanddynamicprogramming/recursion/GetSubsequence.java
@@ -0,0 +1,53 @@
+package src.com.sid.recursionanddynamicprogramming.recursion;
+
+import java.util.ArrayList;
+
+/***
+ * For String of length n
+ *
+ * 2^n Subsequence like : "", a,b,c,ab,ac,bc,abc
+ *
+ * [n(n+1)] / 2 : substring like : a,ab,abc,bc,c
+ *
+ *
+ * ***/
+
+/***
+ * Question
+ * 1. You are given a string str.
+ * 2. Complete the body of getSS function - without changing signature -
+ * to calculate all subsequences of str.
+ * Use sample input and output to take idea about subsequences.
+ *
+ *Sample Input
+ * abc
+ * Sample Output
+ * [, c, b, bc, a, ac, ab, abc]
+ *
+ * **/
+public class GetSubsequence {
+    public static void main(String[] args) throws Exception {
+        String s = "abc";
+        System.out.println(new GetSubsequence().gss(s));
+    }
+
+    public ArrayList<String> gss(String str) {
+
+        //base case
+        if (str.length() == 0) {
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+
+        char ch = str.charAt(0);//retrieve the first element
+        ArrayList<String> rres = gss(str.substring(1));
+        ArrayList<String> mres = new ArrayList<>();
+        for (String s : rres) {
+            mres.add("" + s);
+            mres.add(ch + s);
+        }
+        return mres;
+
+    }
+}
