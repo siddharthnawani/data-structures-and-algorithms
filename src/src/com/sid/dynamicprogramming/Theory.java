5b2546a4344d2d1fee4b4b859ff0142bf2da0cc3
siddharthnawani <siddharth.nawani@gmail.com>
2020-09-27 14:34:41 +0530

    Theory for DP

---
+++ src/src/com/sid/dynamicprogramming/Theory.java
@@ -0,0 +1,24 @@
+package src.com.sid.dynamicprogramming;
+
+public class Theory {
+
+    /**
+     * Three Steps of DP :
+     *
+     * 1. Storage and meaning : What each array item denotes
+     *
+     * 2. Direction Identify : in which direction the problem needs to be solved ,
+     * that is in which direction the array needs to be traversed and solved
+     *
+     * 3. Travel and Solve
+     *
+     * ------
+     *
+     * Insight :
+     *
+     * For duplicacy prepare a 1D dp array ex : target sun subset
+     * for no duplicacy prepare a 1D array : coin change permutation/combination
+     *
+     *
+     * */
+}
