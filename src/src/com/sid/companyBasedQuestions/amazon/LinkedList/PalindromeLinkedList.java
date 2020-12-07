03f033d44b3b336e184d58c34f2992b1b68e15ad
siddharthnawani <siddharth.nawani@gmail.com>
2020-12-08 00:04:01 +0530

    PalindromeLinkedList

---
+++ src/src/com/sid/companyBasedQuestions/amazon/LinkedList/PalindromeLinkedList.java
@@ -0,0 +1,124 @@
+package src.com.sid.companyBasedQuestions.amazon.LinkedList;
+
+/**
+ * https://leetcode.com/problems/palindrome-linked-list/
+ * <p>
+ * 234. Palindrome Linked List
+ * Given a singly linked list, determine if it is a palindrome.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2
+ * Output: false
+ * Example 2:
+ * <p>
+ * Input: 1->2->2->1
+ * Output: true
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ **/
+public class PalindromeLinkedList {
+
+    private ListNode frontPointer;
+
+    private boolean recursivelyCheck(ListNode currentNode) {
+        if (currentNode != null) {
+            if (!recursivelyCheck(currentNode.next)) return false;
+            if (currentNode.val != frontPointer.val) return false;
+            frontPointer = frontPointer.next;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome_USingRecursion(ListNode head) {
+        frontPointer = head;
+        return recursivelyCheck(head);
+    }
+
+    //using iteration 1
+
+
+    public boolean isPalindrome_UsingIteratin1(ListNode head) {
+
+        if (head == null) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    //Using iteration 2
+
+    public boolean isPalindrome_UsingIteration2(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+
+}
