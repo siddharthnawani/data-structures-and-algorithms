8623db8be23fd203050519a0a9edd1e24e158e04
siddharthnawani <siddharth.nawani@gmail.com>
2020-12-10 19:31:52 +0530

    KthSmallestElementinaBST

--- src/src/com/sid/tree/binarySearchTree/KthSmallestElementinaBST.java
+++ src/src/com/sid/tree/binarySearchTree/KthSmallestElementinaBST.java
@@ -3,44 +3,46 @@ package src.com.sid.tree.binarySearchTree;
 
 /**
  * https://leetcode.com/problems/kth-smallest-element-in-a-bst/submissions/
- *
+ * <p>
  * 230. Kth Smallest Element in a BST
  * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: root = [3,1,4,null,2], k = 1
- *    3
- *   / \
- *  1   4
- *   \
- *    2
+ * 3
+ * / \
+ * 1   4
+ * \
+ * 2
  * Output: 1
- *
- * ***/
+ ***/
+
 import java.util.Stack;
 
-public class KthSmallestElementinaBST {
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    TreeNode() {
     }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class KthSmallestElementinaBST {
+
 
     int counter = 0, v = 0;
 
