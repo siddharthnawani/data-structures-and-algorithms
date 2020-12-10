755f974a434180258d37b418b9e426c6ca419899
siddharthnawani <siddharth.nawani@gmail.com>
2020-12-10 19:32:30 +0530

    ValidateBinarySearchTree

--- src/src/com/sid/companyBasedQuestions/amazon/TreesAndGraphs/ValidateBinarySearchTree.java
+++ src/src/com/sid/companyBasedQuestions/amazon/TreesAndGraphs/ValidateBinarySearchTree.java
@@ -19,26 +19,27 @@ import java.util.Stack;
  * Explanation: The root node's value is 5 but its right child's value is 4.
  *
  * **/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class ValidateBinarySearchTree {
 
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 
     public boolean isValidBST_Recursive(TreeNode root) {
         return helper(root, null, null);
