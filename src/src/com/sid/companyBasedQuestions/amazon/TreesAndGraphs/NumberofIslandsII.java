7c63b86b0f9db67a0480253401bbbad606b4a057
siddharthnawani <siddharth.nawani@gmail.com>
2020-12-28 19:53:01 +0530

    NumberofIslandsII

---
+++ src/src/com/sid/companyBasedQuestions/amazon/TreesAndGraphs/NumberofIslandsII.java
@@ -0,0 +1,114 @@
+package src.com.sid.companyBasedQuestions.amazon.TreesAndGraphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/number-of-islands-ii/
+ * 305. Number of Islands II
+ * A 2d grid map of m rows and n columns is initially filled with water. We may perform an addLand operation which turns the water at position (row, col) into a land. Given a list of positions to operate, count the number of islands after each addLand operation. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ * <p>
+ * Example:
+ * <p>
+ * Input: m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]]
+ * Output: [1,1,2,3]
+ **/
+public class NumberofIslandsII {
+    class UnionFind {
+        int count; //#no of connected components
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) { //for problem 200
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+
+        }
+
+        public UnionFind(int N) { //for problem 305 and others
+            count = 0;
+            parent = new int[N];
+            rank = new int[N];
+            for (int i = 0; i < N; i++) {
+                parent[i] = -1;
+                rank[i] = 0;
+            }
+        }
+
+        public boolean isValid(int i) { //for problem 305
+            return parent[i] >= 0; // or parent != -1
+        }
+
+        public void setParent(int i) {
+            parent[i] = i;
+            ++count;
+        }
+
+        public int find(int i) { // path compression
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) { //union with rank
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) parent[rooty] = rootx;
+                else if (rank[rooty] > rank[rootx]) parent[rootx] = rooty;
+                else {
+                    parent[rooty] = rootx;
+                    rank[rootx] += 1;
+                }
+                --count;
+
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+
+    }
+
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        List<Integer> ans = new ArrayList<>();
+        UnionFind uf = new UnionFind(m * n);
+
+        for (int[] pos : positions) {
+            int r = pos[0], c = pos[1];
+            int index = r * n + c;
+            if (uf.parent[index] != -1) {
+                ans.add(uf.count);
+                continue;
+            }
+
+            List<Integer> overlappingNeighbors = new ArrayList<>();
+
+            if (r - 1 >= 0 && uf.isValid((r - 1) * n + c)) overlappingNeighbors.add((r - 1) * n + c);
+            if (r + 1 < m && uf.isValid((r + 1) * n + c)) overlappingNeighbors.add((r + 1) * n + c);
+            if (c - 1 >= 0 && uf.isValid(r * n + c - 1)) overlappingNeighbors.add(r * n + c - 1);
+            if (c + 1 < n && uf.isValid(r * n + c + 1)) overlappingNeighbors.add(r * n + c + 1);
+
+
+            uf.setParent(index);
+            for (int i : overlappingNeighbors) uf.union(i, index);
+            ans.add(uf.getCount());
+
+        }
+
+        return ans;
+    }
+}
