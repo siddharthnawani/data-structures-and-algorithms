c51aa1c1d9b02a4804bc636227f2ca1cf7d8938b
Siddharth Nawani <50983576+siddharthnawani@users.noreply.github.com>
2023-05-28 17:18:17 +0000

    SubarraySumEqualsK

--- src/src/com/2023/data-structures/strings/SubarraySumEqualsK.java
+++ src/src/com/2023/data-structures/strings/SubarraySumEqualsK.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumEqualsK{
-    public static void main(String args[]){
-        System.out.println("Hello");
-    }
-}
+
+    public int subarraySum(int[] nums, int k) {
+       
+        //cal prefix sum
+        int len=nums.length,sum=0,count=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+ 
+        for(int i=0;i<len;i++){
+            sum +=nums[i];
+            if(map.containsKey(sum-k)){
+                count += map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+ 
+         return count;
+     }
+}git statu
